5a12a446ec7df4453614780987237c28c2819df0
Danhschool <116479213+Danhschool@users.noreply.github.com>
2023-05-03 17:34:56 +0700

    Update DataAccessObject.java

--- src/Final/DataAccessObject.java
+++ src/Final/DataAccessObject.java
@@ -3,16 +3,8 @@ package Final;
 import java.util.ArrayList;
 
 public interface DataAccessObject<T> {
-	public int insert(T t);
-	public int  update(T t);
-	public int delete(T t);
+	public int insert(T t) ;
 	
-	public ArrayList<T> selectAll();
-	
-	public T selectById(T t);
-	
-	public ArrayList<T> selectByCondition(String condition);
-	
-	public ArrayList<T> selectSort();
+	public ArrayList<T> selectSort() ;
 	
 }
